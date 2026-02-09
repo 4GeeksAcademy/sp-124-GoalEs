e15b4904f0ae71db9e0429fc0408f72b05ac60e2
sarah-irc <sarahcardoso.sc@gmail.com>
2026-02-09 09:11:52 +0000

    included message crud back/front

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -43,7 +43,8 @@ export const Home = () => {
 				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
 				
 			</p>
-			<button className="btn btn-primary" onClick={() => navigate ("/users")}>Go to Users</button>
+			<button className="btn btn-primary" onClick={() => navigate ("/users")}>Go to Users</button> 
+			<button className="btn btn-primary" onClick={() => navigate ("/messages")}>Go to Message</button>
 			<button className="btn btn-primary ms-3" onClick={() => navigate("/coaches")}>Go to Coaches</button>
 			<button className="btn btn-primary ms-3" onClick={() => navigate("/courses")}>Go to courses</button>
 
